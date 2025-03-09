0d939947671bf4e1f5d15518492f4a54905f3e25
MmD <mmdj27634@gmail.com>
2025-03-09 11:22:06 +0330

    add : files plugin

--- src/bot.ts
+++ src/bot.ts
@@ -1,18 +1,19 @@
-import { Bot, InlineKeyboard, session } from "grammy"
-import { Reactions } from "@grammyjs/emoji"
-import { Conversation, conversations, createConversation } from "@grammyjs/conversations"
+import { Bot, session } from "grammy"
+import { conversations } from "@grammyjs/conversations"
 
-import sleep from "@/utils/sleep"
 import logger from "@/utils/logger"
 
 import env from "@/config/env"
 
 import { type BotContext } from "@/types/context"
 import { type SessionData } from "@/types/session"
+import { hydrateFiles } from "@grammyjs/files"
 
 
 const main = async () => {
     const bot = new Bot<BotContext>(env.BOT_TOKEN)
+
+    bot.api.config.use(hydrateFiles(env.BOT_TOKEN))
 
     const initial = (): SessionData => {
         return {}
@@ -21,8 +22,26 @@ const main = async () => {
     bot.use(session({ initial }))
     bot.use(conversations())
 
+    bot.on(":video", async (ctx) => {
+        await ctx.reply("👍")
+
+        const file = await ctx.getFile()
+
+        console.log(file)
+
+        const path = await file.download("./temp.mp4")
+
+        console.log(path)
+    })
+
+    bot.command("start", async (ctx) => {
+        await ctx.reply("Hello!")
+    })
+
     const botName = await bot.api.getMyName()
     logger.info(`${botName.name} started`)
+
+    bot.start()
 }
 
 main()
--- src/types/context.ts
+++ src/types/context.ts
@@ -1,5 +1,6 @@
 import type { Context, SessionFlavor } from "grammy";
 import type { SessionData } from "./session";
 import type { ConversationFlavor } from "@grammyjs/conversations";
+import { FileFlavor } from "@grammyjs/files";
 
-export type BotContext = ConversationFlavor<Context & SessionFlavor<SessionData>>;
+export type BotContext = FileFlavor<ConversationFlavor<Context & SessionFlavor<SessionData>>>;
