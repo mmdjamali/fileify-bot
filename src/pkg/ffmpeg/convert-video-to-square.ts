5b07abc191c66b792914e0ea2d2a4fb9dcab746e
MmD <mmdj27634@gmail.com>
2025-03-11 12:02:46 +0330

    modify video processing for video notes

--- src/pkg/ffmpeg/convert-video-to-square.ts
+++ src/pkg/ffmpeg/convert-video-to-square.ts
@@ -3,7 +3,12 @@ import ffmpeg from "fluent-ffmpeg";
 export const convertVideoToSquare = (inputPath: string, outputPath: string): Promise<void> => {
     return new Promise((resolve, reject) => {
         ffmpeg(inputPath)
-            .videoFilters("crop=min(iw\\,ih):min(iw\\,ih)")
+            .videoFilters([
+                "crop=min(iw\\,ih):min(iw\\,ih)",
+                "scale=360:360",
+                "format=yuv420p",
+                "setsar=1",
+            ])
             .outputOptions(["-c:v libx264", "-profile:v baseline", "-level 3.0", "-pix_fmt yuv420p"])
             .on("end", () => resolve())
             .on("error", (err) => reject(err))
