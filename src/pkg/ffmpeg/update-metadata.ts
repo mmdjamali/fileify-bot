9ece04cc8ddfa3b11233b7e867a511cd6e84aecf
MmD <mmdj27634@gmail.com>
2025-03-13 16:25:34 +0330

    add : function to change music file's metadata with ffmpeg

---
+++ src/pkg/ffmpeg/update-metadata.ts
@@ -0,0 +1,32 @@
+import Ffmpeg from "fluent-ffmpeg";
+
+const updateMetadata = async (
+    inputPath: string,
+    outputPath: string,
+    metadata: { title?: string; artist?: string; album?: string },
+    coverImagePath?: string
+) => {
+    return new Promise((resolve, reject) => {
+        const command = Ffmpeg(inputPath);
+
+        if (metadata.title) command.outputOptions('-metadata', `title=${metadata.title}`);
+        if (metadata.artist) command.outputOptions('-metadata', `artist=${metadata.artist}`);
+        if (metadata.album) command.outputOptions('-metadata', `album=${metadata.album}`);
+
+        if (coverImagePath) {
+            command
+                .input(coverImagePath)
+                .outputOptions('-map', '0')
+                .outputOptions('-map', '1')
+                .outputOptions('-c', 'copy')
+                .outputOptions('-disposition:v', 'attached_pic');
+        }
+
+        command
+            .save(outputPath)
+            .on('end', resolve)
+            .on('error', reject);
+    });
+}
+
+export default updateMetadata;
